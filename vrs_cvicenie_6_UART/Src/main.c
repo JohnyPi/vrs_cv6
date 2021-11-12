c9ae1a8c1e1f03dfa14124d0c0f74c850cffc5c5
JohnyPi <84911328+JohnyPi@users.noreply.github.com>
2021-11-12 19:34:15 +0100

    added buffer shifting

--- vrs_cvicenie_6_UART/Src/main.c
+++ vrs_cvicenie_6_UART/Src/main.c
@@ -1,131 +1,97 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usart.h"
 #include "gpio.h"
+#include "string.h"
 
+#define BUFF_LEN 20
+
+char buffer[BUFF_LEN];
+char led_state='0';
 
 void SystemClock_Config(void);
 
 void process_serial_data(uint8_t ch);
 
-int main(void)
-{
+int main(void) {
+	memset(buffer, '\0', BUFF_LEN);
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+	NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	SystemClock_Config();
 
-  SystemClock_Config();
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
 
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-
-  USART2_RegisterCallback(process_serial_data);
-
-  char tx_data = 'a';
-
-  while (1)
-  {
-	  LL_USART_TransmitData8(USART2, tx_data++);
-	  tx_data == ('z' + 1) ? tx_data = 'a' : tx_data;
-
-	  LL_mDelay(50);
-  }
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
-
-  if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
-  {
-  Error_Handler();  
-  }
-  LL_RCC_HSI_Enable();
-
-   /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1)
-  {
-    
-  }
-  LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB1_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
-
-   /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
-  {
-  
-  }
-  LL_Init1msTick(8000000);
-  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
-  LL_SetSystemCoreClock(8000000);
-}
+	USART2_RegisterCallback(process_serial_data);
 
 
-void process_serial_data(uint8_t ch)
-{
-	static uint8_t count = 0;
+	while (1) {
+		LL_USART_TransmitData8(USART2, led_state);
+		LL_USART_TransmitData8(USART2, '\r');
 
-	if(ch == 'a')
-	{
-		count++;
-
-		if(count >= 3)
-		{
-			if((LL_GPIO_ReadInputPort(GPIOB) & (1 << 3)) >> 3)
-			{
-				LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3);
-			}
-			else
-			{
-				LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_3);
-			}
-
-			count = 0;
-			return;
-		}
+		LL_mDelay(5000);
 	}
 }
 
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+	if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0) {
+		Error_Handler();
+	}
+	LL_RCC_HSI_Enable();
+
+	/* Wait till HSI is ready */
+	while (LL_RCC_HSI_IsReady() != 1) {
+
+	}
+	LL_RCC_HSI_SetCalibTrimming(16);
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+	LL_RCC_SetAPB2Prescaler(LL_RCC_APB1_DIV_1);
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+	/* Wait till System clock is ready */
+	while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI) {
+
+	}
+	LL_Init1msTick(8000000);
+	LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+	LL_SetSystemCoreClock(8000000);
+}
+
+void process_serial_data(uint8_t ch) {
+
+	if (ch == 0x0D) {
+		return;
+	}
+
+	//robime shift
+	if (strlen(buffer) <= BUFF_LEN) {
+		for (uint8_t i = 1; i < BUFF_LEN; i++) {
+			buffer[i - 1] = buffer[i];
+		}
+		buffer[BUFF_LEN - 1] = '\0';
+	}
+
+	return;
+}
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
